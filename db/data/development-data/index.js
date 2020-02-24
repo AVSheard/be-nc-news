73f9fccc18cfc41e62b656e3aae70d801f4fdea7
AVSheard <a_sheard@yahoo.co.uk>
2020-02-24 12:00:07 +0000

    started makingindex files and getting gitignore to work

---
+++ db/data/development-data/index.js
@@ -0,0 +1,6 @@
+/** @format */
+
+exports.articleData = require("./articles");
+exports.commentsData = require("./comments");
+exports.topicsData = require("./topics");
+exports.usersData = require("./users");
